0638b3a6ecca5fe7b9e7d58e6fb657a1060b7798
stephenwandah <74459143+stephenwandah@users.noreply.github.com>
2021-01-13 04:45:36 -0800

    Add files via upload

---
+++ travel/script.js
@@ -0,0 +1,7 @@
+function openForm(argument) {
+	document.getElementByID("myForm").style.display = "block";
+}
+
+function closeForm(argument) {
+	document.getElementByID("myForm").style.display = "none";
+}
